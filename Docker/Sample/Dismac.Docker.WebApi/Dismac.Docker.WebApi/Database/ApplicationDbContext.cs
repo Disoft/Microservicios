9efaa8d03ff1a984be83f517f3688856761c1aff
jmostajo <jmostajo@info-arch.com>
2025-03-15 15:49:23 -0400

    The latest commit fro Dismac training...

---
+++ Docker/Sample/Dismac.Docker.WebApi/Dismac.Docker.WebApi/Database/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Dismac.Docker.WebApi.Models;
+
+namespace Dismac.Docker.WebApi.Database
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+			try
+			{
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(dbCreator != null)
+                {
+                    if (!dbCreator.CanConnect())
+                        dbCreator.Create();
+                    if(!dbCreator.HasTables())
+                        dbCreator.CreateTables();
+                }
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Docker/Sample/Dismac.Docker.WebApi/Dismac.Docker.WebApi/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dismac.Docker.WebApi.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
